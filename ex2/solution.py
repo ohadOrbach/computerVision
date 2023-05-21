228846c9a45e35d9b1b5d7892d5ae7c623b01c8d
Ohad Orbach <ohadorbach92@gmail.com>
2023-05-21 10:29:06 +0300

    update

--- ex2/solution.py
+++ ex2/solution.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 from scipy.ndimage import uniform_filter
+from tqdm import tqdm
 
 
 def census_transform(img, window_size=(7, 9)):
@@ -14,7 +15,7 @@ def census_transform(img, window_size=(7, 9)):
     rows = np.arange(-(window_height // 2), window_height // 2 + 1)[:, np.newaxis]
     cols = np.arange(-(window_width // 2), window_width // 2 + 1)
 
-    for i in range(window_height // 2, height - window_height // 2):
+    for i in tqdm(range(window_height // 2, height - window_height // 2)):
         for j in range(window_width // 2, width - window_width // 2):
             window = img[i + rows, j + cols]
             center = window[window_height // 2, window_width // 2]
@@ -32,7 +33,7 @@ def cost_volume(left_census, right_census, max_disp):
 
     cost_volume_L = np.zeros((height, width, max_disp), dtype=np.uint8)
     cost_volume_R = np.zeros((height, width, max_disp), dtype=np.uint8)
-    for d in range(max_disp):
+    for d in tqdm(range(max_disp)):
         right_census_shifted = np.roll(right_census, d, axis=1)
         cost_volume_L[:, :, d] = np.sum(left_census != right_census_shifted, axis=2)
         left_census_shifted = np.roll(left_census, -d, axis=1)
@@ -69,7 +70,7 @@ def left_right_consistency_test(disp_left, disp_right, threshold = 1):
 def depth(f, b, disp):
     result = np.zeros_like(disp)
 
-    for i in range(len(disp)):
+    for i in tqdm(range(len(disp))):
         for j in range(len(disp[0])):
             if disp[i][j] != 0:
                 result[i][j] = f*b / disp[i][j]
@@ -78,13 +79,13 @@ def depth(f, b, disp):
     return result
 
 
-imgL = cv2.imread('C:\\Users\\ohad\\Desktop\\CS\\MSc\\cv\\exe1\\ex2\\example\\im_left.jpg', cv2.IMREAD_GRAYSCALE)
-imgR = cv2.imread('C:\\Users\\ohad\\Desktop\\CS\\MSc\\cv\\exe1\\ex2\\example\\im_right.jpg', cv2.IMREAD_GRAYSCALE)
+imgL = cv2.imread('example/im_left.jpg', cv2.IMREAD_GRAYSCALE)
+imgR = cv2.imread('example/im_right.jpg', cv2.IMREAD_GRAYSCALE)
 
-with open('C:\\Users\\ohad\\Desktop\\CS\\MSc\\cv\\exe1\\ex2\\example\\max_disp.txt') as f:
+with open('example/max_disp.txt') as f:
     max_disp = int(f.read())
 
-K = np.loadtxt('C:\\Users\\ohad\\Desktop\\CS\\MSc\\cv\\exe1\\ex2\\example\\K.txt')
+K = np.loadtxt('example/K.txt')
 focal_length = K[0][0]
 
 base_line = 10
@@ -101,19 +102,8 @@ disp_right = proc(costVR, filter)
 
 dl, dr = left_right_consistency_test(disp_left, disp_right)
 
-
-cv2.imshow('Disparity left', dl/ np.max(dl)*255)
-cv2.imshow('Disparity right', dr/ np.max(dr)*255)
-cv2.waitKey(0)
-
 depth_left = depth(focal_length, base_line, dl)
 depth_right = depth(focal_length, base_line, dr)
-
-print(np.max(disp_left), np.max(dl), np.max(depth_left))
-
-cv2.imshow('depth left', depth_left/ np.max(depth_left))
-cv2.imshow('depth right', depth_right/ np.max(depth_right)*255)
-cv2.waitKey(0)
 
 cv2.imwrite(f'disp_l.jpg', dl/ np.max(dl)*255)
 cv2.imwrite(f'disp_r.jpg', dr/ np.max(dr)*255)
