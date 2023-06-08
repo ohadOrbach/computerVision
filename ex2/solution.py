a94099ee8bad47beda0055a0c760a5185c634d9f
AmittBarak <amitbarak1997@gmail.com>
2023-06-08 03:46:51 +0300

    Final!!

--- ex2/solution.py
+++ ex2/solution.py
@@ -1,8 +1,9 @@
+import os
+
 import cv2
 import numpy as np
 from scipy.ndimage import uniform_filter
 from tqdm import tqdm
-import os
 
 
 def census_transform(img, window_size=(7, 9)):
@@ -53,7 +54,8 @@ def proc(cost_volume, filter_size=(5, 5)):
     cc3 = min_indices.astype(np.uint8)
     return cc3
 
-def left_right_consistency_test(disp_left, disp_right, threshold = 1):
+
+def left_right_consistency_test(disp_left, disp_right, threshold=1):
     rows, cols = np.indices(disp_left.shape)
 
     x_coords = np.clip((cols - disp_left), 0, 1023)
@@ -68,16 +70,18 @@ def left_right_consistency_test(disp_left, disp_right, threshold = 1):
 
     return disp_left, disp_right
 
-def depth(f, b, disp):
-    result = np.zeros_like(disp)
 
-    for i in tqdm(range(len(disp))):
+def depth(f, b, disp):
+    result = np.zeros_like(disp, dtype=np.float64)
+
+    for i in tqdm(range(len(disp)), desc="Depth"):
         for j in range(len(disp[0])):
             if disp[i][j] != 0:
-                result[i][j] = f*b / disp[i][j]
+                result[i][j] = f * b / disp[i][j]
             else:
                 result[i][j] = 0
     return result
+
 
 def read_intrinsic_matrix(file_path):
     with open(file_path, 'r') as f:
@@ -90,74 +94,52 @@ def read_intrinsic_matrix(file_path):
 
     return intrinsic_matrix
 
-def generate_novel_views(left_image, left_depth_map, right_depth_map, intrinsics_matrix, baseline,
-                         num_views=11):
-    height, width = left_image.shape[:2]
-    novel_views = []
 
+def generate_novel_views(left_image, left_depth_map, intrinsics_matrix, baseline, num_views=11):
+    height, width = left_image.shape[:2]
     baseline_interval = 0.01  # 1 cm intervals on the baseline
     for i in tqdm(range(num_views)):
-        # Compute the translation vector for each camera position
         translation = np.array([baseline - (num_views - i) * baseline_interval, 0, 0])
-
-        # Compute the extrinsics matrix for each camera position
         extrinsics = np.eye(4)
         extrinsics[:3, 3] = translation
-
-        # Compute the inverse of the extrinsics matrix
         extrinsics_inv = np.linalg.inv(extrinsics)
-
         # Calculate reprojection of all image coordinates to 3D
         points_3d_left = np.zeros((height, width, 3))
-        points_3d_right = np.zeros((height, width, 3))
         valid_indices = np.where(~np.isnan(left_depth_map))
 
         for y, x in zip(valid_indices[0], valid_indices[1]):
             left_depth = left_depth_map[y, x]
-            right_depth = right_depth_map[y, x]
-
             # Compute the 3D point in the left camera coordinate system
             point_2d = np.array([x, y, 1])
             point_3d_left = np.dot(np.linalg.inv(intrinsics_matrix), point_2d) * left_depth
             points_3d_left[y, x] = point_3d_left
-
-            # Compute the 3D point in the right camera coordinate system
-            point_3d_right = np.dot(np.linalg.inv(intrinsics_matrix), point_2d) * right_depth
-            points_3d_right[y, x] = point_3d_right
-
         # Drop the 3D points back onto the left camera plane
         novel_view = np.zeros_like(left_image)
         valid_indices_3d = np.where(points_3d_left[:, :, 2] > 0)
 
         for y, x in zip(valid_indices_3d[0], valid_indices_3d[1]):
             point_3d_left = points_3d_left[y, x]
-
-            # Transform the 3D point to the left camera frame
             point_3d_left = np.dot(extrinsics_inv, np.append(point_3d_left, 1))[:3]
-
-            # Project the 3D point onto the left camera plane
             left_pixel = np.dot(intrinsics_matrix, point_3d_left)
             left_pixel /= left_pixel[2]
-
             left_x = int(round(left_pixel[0]))
             left_y = int(round(left_pixel[1]))
 
             if 0 <= left_x < width and 0 <= left_y < height:
                 novel_view[left_y, left_x] = left_image[y, x]
 
-        novel_views.append(novel_view)
+        yield novel_view
 
-    return novel_views
 
-directories = ['set_1', 'set_2', 'set_3', 'set_4', 'set_5']
+directories = ['set_2', 'set_3', 'set_4', 'set_5']
 
 for directory in directories:
 
     imgL = cv2.imread(os.path.join(directory, 'im_left.jpg'), cv2.IMREAD_GRAYSCALE)
     imgR = cv2.imread(os.path.join(directory, 'im_right.jpg'), cv2.IMREAD_GRAYSCALE)
 
-    imgLcolor = cv2.imread(os.path.join(directory, 'im_left.jpg'),  cv2.IMREAD_COLOR)
-    imgRcolor = cv2.imread(os.path.join(directory, 'im_right.jpg'),  cv2.IMREAD_COLOR)
+    imgLcolor = cv2.imread(os.path.join(directory, 'im_left.jpg'), cv2.IMREAD_COLOR)
+    imgRcolor = cv2.imread(os.path.join(directory, 'im_right.jpg'), cv2.IMREAD_COLOR)
 
     with open(os.path.join(directory, 'max_disp.txt')) as f:
         max_disp = int(f.read())
@@ -167,8 +149,8 @@ for directory in directories:
 
     base_line = 10
 
-    window_shape = (11,13)
-    filter = (7,7)
+    window_shape = (11, 13)
+    filter = (7, 7)
     censusL = census_transform(imgL, window_shape)
     censusR = census_transform(imgR, window_shape)
     costVL, costVR = cost_volume(censusL, censusR, max_disp)
@@ -179,22 +161,21 @@ for directory in directories:
     dl, dr = left_right_consistency_test(disp_left, disp_right)
 
     depth_left = depth(focal_length, base_line, dl)
+    depth_left = depth_left / np.max(depth_left) * 255
     depth_right = depth(focal_length, base_line, dr)
-
-    novel_views = generate_novel_views(imgLcolor, depth_left, depth_right, K, base_line)
+    depth_right = depth_right / np.max(depth_right) * 255
+    novel_views = generate_novel_views(imgLcolor, depth_left, K, 0.1)
 
     output_dir = os.path.join('results', directory)
-    #os.makedirs(output_dir, exist_ok=True)
+    os.makedirs(output_dir, exist_ok=True)
 
-    #cv2.imwrite(os.path.join(output_dir,'disp_l.jpg'), dl/ np.max(dl)*255)
-    #cv2.imwrite(os.path.join(output_dir,'disp_r.jpg'), dr/ np.max(dr)*255)
+    cv2.imwrite(os.path.join(output_dir, 'disp_l.jpg'), dl / np.max(dl) * 255)
+    cv2.imwrite(os.path.join(output_dir, 'disp_r.jpg'), dr / np.max(dr) * 255)
 
-    #cv2.imwrite(os.path.join(output_dir,'depth_l.jpg'),  depth_left/ np.max(depth_left)*255)
-    #cv2.imwrite(os.path.join(output_dir,'depth_r.jpg'), depth_right/ np.max(depth_right)*255)
+    cv2.imwrite(os.path.join(output_dir, 'depth_l.jpg'), depth_left)
+    cv2.imwrite(os.path.join(output_dir, 'depth_r.jpg'), depth_right)
 
-    for i, novel_view in enumerate(novel_views):
-        cv2.imwrite(os.path.join(output_dir,f'synth_{i + 1}.jpg'), novel_view)
-
-
-
-
+    count = 0
+    for novel_view in novel_views:
+        cv2.imwrite(os.path.join(output_dir, f'synth_{count + 1}.jpg'), novel_view)
+        count += 1
